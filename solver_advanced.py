fbb3ce5b7775e66d2fc961fe2b06f8bf01da062f
Teryx-Enzo <71768408+Teryx-Enzo@users.noreply.github.com>
2024-02-04 10:52:53 -0500

    premier_essai
    fun fact, ça marche pas

--- solver_advanced.py
+++ solver_advanced.py
@@ -1,4 +1,5 @@
 from utils import *
+import numpy as np
 
 class CustomWall(Wall):
 
@@ -7,14 +8,25 @@ class CustomWall(Wall):
     """
     def __init__(self, w, h):
         super().__init__(w, h)
+        self.matrix_place = np.zeros((w,h))
         self.max_rect = (w,h)
         self.coord_max_rect = (0,0)
 
-    def maj_ajout_mur(self, art_piece_id,x,y,w,h):
-        self._artpieces[art_piece_id] = ()
+    def maj_ajout_artpiece(self, art_piece,x,y):
+        print(self.matrix_place)
+        #ajout du tableau au dictionnaire du mur
+        self._artpieces[art_piece.get_idx()] = (x,y)
+
+        #mise a jour de la matrice de masque
+        w,h = art_piece.width(), art_piece.height()
+        self.matrix_place[x:x+w,y:y+h] += 1
+        self.max_rect, self.coord_max_rect = self.maximalRectangle()
     
 
-    def maximalRectangle(matrix):
+    def maximalRectangle(self):
+        matrix = self.matrix_place
+
+        
     
 
         def largestRectangleArea(heights):
@@ -54,8 +66,11 @@ class CustomWall(Wall):
                 max_area = current_area
                 max_area_coordinates = (current_coordinates[0], i - current_coordinates[1] + 1, current_coordinates[2], i)
 
-        return max_area, max_area_coordinates
 
+        return (max_area_coordinates[2]-max_area_coordinates[0],max_area_coordinates[3]-max_area_coordinates[1]), (max_area_coordinates[0], max_area_coordinates[1])
+
+
+    
     
 
     
@@ -69,4 +84,38 @@ def solve(instance: Instance) -> Solution:
         Solution: A solution object initialized with 
                   a list of tuples of the form (<artipiece_id>, <wall_id>, <x_pos>, <y_pos>)
     """
-    raise Exception('This should be implemented')
+    walls = []
+    wallw, wallh = instance.wall.width(), instance.wall.height()
+
+    #Pour chaque oeuvre on essaye de la mettre sur un mur deja occupe
+    for id, art_piece in instance.artpieces_dict.items():
+        
+        placed = False
+        w,h  = art_piece.width(), art_piece.height()
+        
+        #Pour chaque oeuvre on essaye de la mettre sur un mur deja occupe
+        for wall in walls:
+            if not placed :
+
+                x,y = wall.coord_max_rect
+                print(x,y)
+                W,H = wall.max_rect
+                if w <= W and h<= H:
+
+                    wall.maj_ajout_artpiece(art_piece,x,y)
+                    placed = True
+
+
+        #sinon on la met sur un nouveau mur
+        if not placed:
+            walls.append(CustomWall(wallw,wallh))
+            walls[-1].maj_ajout_artpiece(art_piece,0,0)
+
+    
+
+        
+    solution = []
+    for i in range(len(walls)):
+        solution += walls[i].gen_for_solution(i)
+
+    return Solution(solution)
--- solver_naive.py
+++ solver_naive.py
@@ -10,4 +10,5 @@ def solve(instance: Instance) -> Solution:
         Solution: A solution object initialized with 
                   a list of tuples of the form (<artipiece_id>, <wall_id>, <x_pos>, <y_pos>)
     """
+    print(instance.wall)
     return Solution([(i,i,0,0) for i in instance.artpieces_dict.keys()])
